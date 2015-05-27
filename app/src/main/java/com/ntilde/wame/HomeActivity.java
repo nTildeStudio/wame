7e8f4d08cfd95a42c5615281a70b6f9d3d5b0d5b
Carlos Domínguez <3002789@insags.com>
2015-05-27 16:42:28 +0200

    Primera aproximación

--- app/src/main/java/com/ntilde/wame/HomeActivity.java
+++ app/src/main/java/com/ntilde/wame/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.ntilde.wame;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+
+import com.google.gson.Gson;
+import com.ntilde.wame.model.Levels;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    protected static int nextLevel=0;
+    protected static Levels levels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,35 @@ public class HomeActivity extends ActionBarActivity {
         findViewById(R.id.home_play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nextLevel=0;
                 HomeActivity.this.startActivity(new Intent(HomeActivity.this, GameActivity.class));
             }
         });
+
+        levels = loadLevels();
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextLevel=1;
+                HomeActivity.this.startActivity(new Intent(HomeActivity.this, GameActivity.class));
+            }
+        });
+    }
+
+    public Levels loadLevels() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("levels.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return new Gson().fromJson(json,Levels.class);
     }
 
 }
